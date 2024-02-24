95358126edd7994937303dd27765628fbdf3fa71
PELENIUM <i.d.chernyshov@yandex.ru>
2024-02-24 23:23:36 +0300

    removed useless stuff

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
 from ultralytics import YOLO
 import cv2
-import math 
-# start webcam
+import math
+
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
-# model
 model = YOLO("yolo-Weights/yolov8n.pt")
-
-# object classes
-classNames = ["person",
-              # "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              #"traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              #"dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              #"handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              #"baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              #"fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              #"carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              #"diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              #"microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              #"teddy bear", "hair drier", "toothbrush"
-              ]
-
 
 while True:
     peopleCounter = 0
@@ -30,38 +14,34 @@ while True:
     success, img = cap.read()
     results = model(img, stream=True)
 
-    # coordinates
     for r in results:
         boxes = r.boxes
 
         for box in boxes:
             cls = int(box.cls[0])
+            # 0 = human
             if cls == 0:
                 peopleCounter += 1
-                # bounding box
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-                # put box in cam
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
                 cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-                # confidence
                 confidence = math.ceil((box.conf[0]*100))/100
-                print("Confidence --->",confidence)
+                print("Person found, Confidence --->", confidence)
 
-                # class name
-                print("Class name -->", classNames[cls])
-
-                # object details
                 org = [x1, y1]
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 fontScale = 1
                 color = (255, 0, 0)
                 thickness = 2
 
-                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
-    
-    cv2.putText(img, f"People in picture: {peopleCounter}", [10, 20], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+                cv2.putText(img, "person", org,
+                            font, fontScale, color, thickness)
+
+    cv2.putText(img, f"People in picture: {peopleCounter}",
+                [10, 20], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
